1a2efbe0ff7dead8aaa6461dee1ac8bcc083e665
unknown <jacob.e.england@gmail.com>
2020-07-21 12:30:45 -0400

    Implemented counting sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -36,16 +36,30 @@ data. The idea behind this algorithm then is that we can create "buckets"
 from 0 up to the max value. This is most easily done by initializing an
 array of 0s whose length is the max value + 1 (why do we need this "+ 1"?).
 
-Each buckets[i] then is responsible for keeping track of how many times 
+Each buckets[i] then is responsible for keeping track of how many times
 we've seen `i` in the input set of data as we iterate through it.
 Once we know exactly how many times each piece of data in the input set
-showed up, we can construct a sorted set of the input data from the 
-buckets. 
+showed up, we can construct a sorted set of the input data from the
+buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
-def counting_sort(arr, maximum=None):
+
+
+def counting_sort(arr, maximum):
     # Your code here
+    buckets = [0] * (maximum + 1)
+    count = 0
+    result = [0] * (len(arr))
+    for i in range(len(arr)):
+        buckets[arr[i]] += 1
+    for i in range(len(buckets)):
+        count += buckets[i]
+        buckets[i] = count
+    for i in range(len(result)):
+        result[buckets[arr[i]] - 1] = arr[i]
+        buckets[arr[i]] -= 1
+    return result
 
 
-    return arr
+counting_sort([1, 5, 7, 3, 3, 2, 1, 6, 4, 3], 7)
